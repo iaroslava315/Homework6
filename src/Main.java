0cf3717067694fd21ddb443a73fdf5c95bf3a73f
iaroslava315 <iaroslava.315@gmail.com>
2026-04-20 14:14:07 +0700

    отформатированный

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 void main() {
 
 //  Task 1
@@ -23,7 +22,7 @@ void main() {
 //  Task 3
     {
         System.out.println("Task 3");
-        for (int i = 0; i <= 17; i = i+2) {
+        for (int i = 0; i <= 17; i = i + 2) {
             System.out.println(i);
         }
     }
@@ -41,7 +40,7 @@ void main() {
 //  Task 5
     {
         System.out.println("Task 5");
-        for (int i = 1904; i <= 2096; i = i+4) {
+        for (int i = 1904; i <= 2096; i = i + 4) {
             System.out.println(i + " год является високосным");
         }
     }
@@ -50,7 +49,7 @@ void main() {
 //  Task 6
     {
         System.out.println("Task 6");
-        for (int i = 7; i <= 98; i = i+7) {
+        for (int i = 7; i <= 98; i = i + 7) {
             System.out.println(i);
         }
     }
@@ -59,7 +58,7 @@ void main() {
 //  Task 7
     {
         System.out.println("Task 7");
-        for (int i = 1; i <= 512; i = i*2) {
+        for (int i = 1; i <= 512; i = i * 2) {
             System.out.println(i);
         }
     }
@@ -83,7 +82,7 @@ void main() {
         int savings = 29000;
         int total = 0;
         for (int i = 1; i <= 12; i++) {
-            total = total + total/100;
+            total = total + total / 100;
             total = total + savings;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
